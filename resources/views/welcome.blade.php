a5cdea082f67bff2ab4d50b47eaf355ca088943f
kgb gil <kgbgil0@gmail.com>
2025-07-19 12:44:52 +0000

    name change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio</title>
+    <link rel="icon" href="/img/Logo-Kogo.png" type="image/png">
+    <link rel="icon" type="image/png" sizes="64x64" href="/img/Logo-Kogo.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/Logo-Kogo.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/Logo-Kogo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gildas Korabalo</title>
     <link rel="stylesheet" href="/style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://unpkg.com/typed.js@2.0.15/dist/typed.umd.js"></script>
-    <link rel="icon" href="/img/Logo-Kogo.png" type="image/png">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
 </head>
