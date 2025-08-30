64c2042327eabfc1f813b05f6e44ac29d043cdb6
kgb gil <kgbgil0@gmail.com>
2025-08-30 17:19:02 +0000

    modification W

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
         <div class="home-content">
             <h3>Hello, It's Me </h3>
             <h1>Gildas KORABALO</h1>
-            <H3 class="and-im-a">And I'm a<br><span class="job-title" style="color: #0ef;">Front-End Developer | Web Developer | WordPress | Business Lawyer</span></H3>
+            <H3 class="and-im-a">And I'm a :<br><span class="job-title" style="color: #0ef;">Front-End Developer | Web Developer | WordPress | Business Lawyer</span></H3>
             <p>I'm a Web Designer with extensive experience for over 3 years.
                 {{-- <br> exepertise is to create and website design,Front-End design , and many more.... --}}
             </p>
